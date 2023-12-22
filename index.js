eb773bc00e1d38c3a961ae5a234ba8e60ea65732
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-12-22 19:29:21 +0600

    post api

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -12,7 +10,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 app.use(express.json());
-app.use(cookieParser());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.onvejqf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+
+        const tasksCollection = client.db("taskiaDb").collection("tasks");
+        app.get('/task/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await tasksCollection.find(query).toArray();
+            res.send(user);
+        })
+        app.post('/task', async (req, res) => {
+            const taskItem = req.body;
+            const result = await tasksCollection.insertOne(taskItem);
+            res.send(result);
+        });
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
@@ -20,9 +56,9 @@ app.use(cookieParser());
 
 
 app.get('/', (req, res) => {
-  res.send('taskia making server is running')
+    res.send('taskia making server is running')
 })
 
 app.listen(port, () => {
-  console.log(`taskia Server is running on port: ${port}`)
+    console.log(`taskia Server is running on port: ${port}`)
 })
